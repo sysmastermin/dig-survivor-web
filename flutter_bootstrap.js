730f215d2f25d32fd3e71c0645123f5b27449aa5
sysmaster <sysmastermin@gmail.com>
2026-08-24 18:34:26 +0900

    Deploy: mineral icons everywhere, full HUD ore strip

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -43,6 +43,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "671301436" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "1640881333" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
 });
