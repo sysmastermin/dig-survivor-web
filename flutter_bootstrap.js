6ff16f0300cc2adf84a467c6ba6beabf45f0ead9
sysmaster <sysmastermin@gmail.com>
2026-08-24 16:20:20 +0900

    Deploy: gear material hints, sealed-mob combat gate, mine sprite, band pacing

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -43,6 +43,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1699268892" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "2014356747" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
 });
