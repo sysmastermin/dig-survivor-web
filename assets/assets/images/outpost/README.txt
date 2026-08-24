074ee9b799a6b8f1e7b54667232f608327bd1bc6
sysmaster <sysmastermin@gmail.com>
2026-08-24 11:25:44 +0900

    Deploy Dig Survivor web build

---
+++ assets/assets/images/outpost/README.txt
@@ -0,0 +1,7 @@
+# 여기에 전초기지 건물 스프라이트를 넣는다.
+# 규격: docs/outpost-art-spec.md
+#   research_lab.png     90x28
+#   ore_depot.png        82x31
+#   rig_works.png        92x32
+#   doctor_booth.png     36x24
+#   alchemist_booth.png  36x24
---
+++ flutter_bootstrap_pixel_platformer_v30.js
@@ -0,0 +1,10 @@
+{{flutter_js}}
+{{flutter_build_config}}
+
+_flutter.buildConfig.builds[0].mainJsPath = 'main.pixel-platformer-terrain-v30.js';
+
+_flutter.loader.load({
+  serviceWorkerSettings: {
+    serviceWorkerVersion: {{flutter_service_worker_version}},
+  },
+});
