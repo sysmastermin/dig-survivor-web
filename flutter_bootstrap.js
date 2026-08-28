778d23f2eec8e04531e1210854df9405f449f533
sysmaster <sysmastermin@gmail.com>
2026-08-29 05:32:33 +0900

    Deploy: objective panel moved right and slimmed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -43,6 +43,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2156904673" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "944881113" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
 });
