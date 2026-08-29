2fc27008d2d06c8118bebe0abe1156670811d13f
sysmaster <sysmastermin@gmail.com>
2026-08-29 16:26:49 +0900

    Deploy: bag on the right, vertical O2, quest metric fixes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -43,6 +43,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3386486468" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "2849686598" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
 });
